253019572f8c44b7a65323f27b8c257092eeb005
ivanbenke <benke.ivan@gmail.com>
2017-03-18 23:09:21 +0100

    Update dashboard_stud.php

--- dashboard_stud.php
+++ dashboard_stud.php
@@ -25,9 +25,9 @@
 <html ng-app="uploadApp">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<script src="angular/angular.js"></script>
-		<script src="angular/angular-route.js"></script>
-		<script src="angular/angular-cookies.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-route.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-cookies.js"></script>
 		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 		<link href="style.css" rel="stylesheet">
 		<script src="js/app_upload.js"></script>
