c6a8fb4ee65160e5865a589cf038d1d37227677a
ivanbenke <benke.ivan@gmail.com>
2017-03-18 23:10:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html ng-app="myApp">
 	<head>
 		<title>Dnevnik rada</title>
-		<script src="angular/angular.js"></script>
-		<script src="angular/angular-route.js"></script>
-		<script src="angular/angular-cookies.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-route.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-cookies.js"></script>
 		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
 		<link href="style.css" rel="stylesheet">
 		<script src="js/app.js"></script>
