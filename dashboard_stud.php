d9363afb493b7a4be1718265237d8b573dc3a2fb
ivanbenke <benke.ivan@gmail.com>
2017-03-18 23:20:05 +0100

    Update dashboard_stud.php

--- dashboard_stud.php
+++ dashboard_stud.php
@@ -28,7 +28,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-route.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular-cookies.js"></script>
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 		<link href="style.css" rel="stylesheet">
 		<script src="js/app_upload.js"></script>
 		<script src="js/ui-bootstrap-tpls-0.14.3.min.js"></script>
